50b8a6f6ecfcabcddb581b6fabbf939aa2da9ad0
Silent Skill <breethly.ios.app@example.com>
2025-06-21 19:39:30 +0200

    export dodany plugin plus dopisane kolekcje

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = () => ({
+  "strapi-csv-import-export": {
+    config: {
+      authorizedExports: [
+        "api::about.about",
+        "api::article.article",
+        "api::author.author",
+        "api::category.category",
+        "api::global.global"
+      ],
+      authorizedImports: [
+        "api::about.about",
+        "api::article.article",
+        "api::author.author",
+        "api::category.category",
+        "api::global.global"
+      ]
+    }
+  }
+});
